c3d736008e3d7272f97ed7557ef2160571641c87
Daniel Lemos <137338318+lemosdaniel@users.noreply.github.com>
2023-11-20 11:15:27 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+for (let i = 99; i > 0; i--) {
+    document.write("<p>" + i + " bottle(s) of beer on the wall, " + i + " bottle(s) of beer.</p>");
+    document.write("<p>Take one down and pass it around, " + (i - 1) + " bottle(s) of beer on the wall.</p>");
+}
+document.write("<p>No more bottles of beer on the wall, no more bottles of beer.</p>");
+document.write("<p>Go to the store and buy some more, 99 bottles of beer on the wall.</p>");
